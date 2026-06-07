033e0401367ad5ee3d498b7fa73b731d1979abdf
bbowman44 <bbjt11@gmail.com>
2026-06-07 00:04:15 -0500

    Change file structure and fix Makefile errors. Also had to squash bugs present with type mismatch

--- Makefile
+++ Makefile
@@ -9,13 +9,13 @@ BINS := $(patsubst examples/%.c,build/%,$(EXAMPLE_SRCS))
 all: $(BINS)
 
 build/%: examples/%.c $(LIB_SRCS) | build
-	$(CC) $(CFLAGS) $(SAN) $(INCLUDE) $< $(LIB_SRCS) -o $@
+	$(CC) $(CFLAGS) $(SAN) $(INCLUDE) examples/$*.c $(LIB_SRCS) -o $@
 
 build:
 	mkdir -p build
 
 run-%: build/%
-	./$
+	./build/$*
 
 clean:
 	rm -rf build
--- lib/stats.c
+++ lib/stats.c
@@ -3,7 +3,7 @@
 int32_t stats_sum(const int32_t *data, size_t n)
 {
     int32_t sum = 0;
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
     {
         sum = data[i] + sum;
     }
@@ -20,7 +20,7 @@ bool stats_min_max(const int32_t *data, size_t n,int32_t *out_min, int32_t *out_
     *out_min = data[0];
     *out_max = data[0];
 
-    for(int i = 0; i < n; i++)
+    for(size_t i = 0; i < n; i++)
     {
         if (data[i] < *out_min)
         {
